6c9d84218e836ec7a23eaeba127728cc2dc5b13d
William Mosquera <wjmmk3@gmail.com>
2024-03-09 09:30:11 -0500

    Setup Files Changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { CounterComponent } from './basic/counter/counter.component';
+import { CharizardComponent } from './basic/charizard/charizard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'basic/counter',
+    component: CounterComponent
+  },
+  {
+    path: 'basic/charizard',
+    component: CharizardComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'basic/counter'
+  }
+];
--- test/app.component.spec.ts
+++ test/app.component.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { AppComponent } from '../src/app/app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -8,19 +8,19 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  test('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    const component = fixture.componentInstance;
+    expect(component).toBeTruthy();
   });
 
-  it(`should have the 'Angular-Testing' title`, () => {
+  test(`should have the 'Angular-Testing' ttestle`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-Testing');
+    const component = fixture.componentInstance;
+    expect(component.title).toEqual('Angular-Testing');
   });
 
-  it('should render title', () => {
+  test('should render ttestle', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
